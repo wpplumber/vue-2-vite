0118dba60b45d52fcb2e8beb0951dcfcc24368c5
wpplumber <abuabdellah.abdurahman@gmail.com>
2022-10-05 15:23:21 +0100

    Justify folders location

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { createPinia, PiniaVuePlugin } from 'pinia'
+
+Vue.use(PiniaVuePlugin)
+const pinia = createPinia()
+
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+    // note the same `pinia` instance can be used across multiple Vue apps on
+  // the same page
+  pinia,
 }).$mount('#app')
